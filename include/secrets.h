449010a1b8d0cc002e24f4f8d3d810445ba3dfc2
wooddie <n1ght.d1ck1ns@gmail.com>
2025-12-29 12:27:04 +0500

    mm

--- include/secrets.h
+++ include/secrets.h
@@ -7,6 +7,7 @@ struct WiFiCred {
 
 #define WIFI_NETWORKS_COUNT 3
 
+//you wifi list (essid and pass)
 const WiFiCred WIFI_NETWORKS[WIFI_NETWORKS_COUNT] = {
     {"MyHomeWiFi",   "real_pass_1"},
     {"OfficeWiFi",   "real_pass_2"},
